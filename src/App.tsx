29d30c4e73e8a705d0c337eeadfbb43af8fdab2f
nothingroft <enderbr4@gmail.com>
2021-12-13 18:53:16 -0300

    Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { Home } from "./pages/Home";
+import { BrowserRouter, Route } from 'react-router-dom'
 
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route path="/" element={<Home />}/>
+      <Route path="/rooms/new" element={<NewRoom />}/>
+    </BrowserRouter> 
   );
 }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,11 +1,10 @@
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg'
-import googleIcon from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss'
 import { Button } from '../components/button';
 
-export function Home() {
+export function NewRoom() {
   return (
     <div id="page-auth">
       <aside>
@@ -16,20 +15,19 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="logo" />
-          <button className="create-room">
-            <img src={googleIcon} alt="" />
-            Crie sua sala Google
-          </button>
-          <div className="separator">ou entre em uma sala</div>
+          <h2>Criar nova sala</h2>
           <form>
             <input
               type="text"
-              placeholder="Digite o código da sala"
+              placeholder="Nome da sala"
             />
             <Button type="submit">
-              Entrar na sala
+              Criar sala
             </Button>
           </form>
+          <p>
+            Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+          </p>
         </div>
       </main>
     </div>
